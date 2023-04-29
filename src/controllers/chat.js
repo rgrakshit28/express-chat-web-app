63ec26c6a02af39f3622ac2696a53f90374968b4
rgrakshit28 <rgrakshit28.professional@gmail.com>
2023-04-29 14:20:55 +0530

    Create Individual and Group Chat API Created

--- src/controllers/chat.js
+++ src/controllers/chat.js
@@ -1,18 +1,74 @@
 import Chat from "../models/chat.js";
+import User from "../models/user.js";
 
-export const createChat = async (req, res) => {
+export const createIndividualChat = async (req, res) => {
   try {
-    const { type, participant } = req.body;
-    const currentUser = req.user._id;
+    const { userId } = req.body;
+    const loggedInUserId = req.user._id;
 
-    const chat = await Chat.create({
-      type,
-      participants: [currentUser, participant],
+    if (userId === loggedInUserId) {
+      return res.status(400).json({ message: "You cannot start a chat with yourself" });
+    }
+
+    const existingChat = await Chat.findOne({
+      type: "Individual",
+      participants: {
+        $all: [userId, loggedInUserId],
+      },
     });
 
-    return res.status(200).json({ message: "Chat created", chat });
+    if (existingChat) {
+      return res.status(400).json({ message: "Chat already exists" });
+    }
+
+    const newChat = await Chat.create({
+      type: "Individual",
+      participants: [userId, loggedInUserId],
+    });
+
+    const user = await User.findById(userId);
+
+    const chat = {
+      chatId: newChat._id,
+      type: newChat.type,
+      participants: newChat.participants,
+      user: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+    };
+
+    res.status(200).json({ message: "Individual Chat Created", chat });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const createGroupChat = async (req, res) => {
+  try {
+    const { avatar, name, about, participants } = req.body;
+    const loggedInUserId = req.user._id;
+
+    const existingUsers = await User.find({ _id: { $in: participants } });
+    if (existingUsers.length !== participants.length) {
+      return res.status(400).json({ message: "One or more participants not found" });
+    }
+
+    participants.push(loggedInUserId);
+
+    const chat = await Chat.create({
+      type: "Group",
+      avatar: avatar || null,
+      name,
+      about,
+      participants,
+      admin: loggedInUserId,
+    });
+
+    res.status(200).json({ message: "Group Chat Created", chat });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyToken } from "../config/auth.js";
-import { createChat } from "../controllers/chat.js";
+import { createIndividualChat, createGroupChat } from "../controllers/chat.js";
 
 const router = express.Router();
 
-router.post("/", verifyToken, createChat);
+router.post("/individual", verifyToken, createIndividualChat);
+router.post("/group", verifyToken, createGroupChat);
 
 export default router;
